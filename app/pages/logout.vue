edeaa08e6bb4c456b11fee3b5fbc21d1faf74f89
MindCollaps <ntill@gmx.de>
2026-06-18 12:55:16 +0200

    🐋 Fix docker build

--- app/pages/logout.vue
+++ app/pages/logout.vue
@@ -1,3 +1,9 @@
-<script lang="ts">
-document.location.replace('/logout');
+<template>
+	<div class="logout-page">Signing out...</div>
+</template>
+
+<script setup lang="ts">
+onMounted(() => {
+	window.location.replace('/logout');
+});
 </script>
